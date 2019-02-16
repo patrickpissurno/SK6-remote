d1956d8a829b8622112c0fb34f8c56b88fc8023c
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-15 22:34:40 -0200

    First working version
    - macOS version of the daemon is finally working
    - Still need to find a way for it to start with the system
    - Still need to implement a Windows version of the daemon

--- daemon/macOS/daemon.cpp
+++ daemon/macOS/daemon.cpp
@@ -1,11 +1,13 @@
-#include <stdio.h>    // Standard input/output definitions
+#include <stdio.h> // Standard input/output definitions
 #include <stdlib.h>
-#include <string.h>   // String function definitions
-#include <unistd.h>   // for usleep()
+#include <string.h> // String function definitions
+#include <unistd.h> // for usleep()
 #include <getopt.h>
 #include <ApplicationServices/ApplicationServices.h>
 
-#include "arduino-serial-lib.h"
+extern "C" {
+    #include "arduino-serial-lib.h"
+}
 
 //from /System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h
 const int kVK_LeftArrow = 0x7B;
@@ -15,7 +17,20 @@ const int kVK_Return = 0x24;
 const int kVK_ANSI_N = 0x2D;
 const int kVK_ANSI_P = 0x23;
 
-void Press(int key, bool shift) {
+const int BUTTON_NEXT = 1;
+const int BUTTON_PREV = 2;
+const int BUTTON_PAUSE = 3;
+const int BUTTON_INFO = 4;
+const int BUTTON_AVSYNC = 5;
+
+const int BUTTON_NEXT_HOLD = 10 + BUTTON_NEXT;
+const int BUTTON_PREV_HOLD = 10 + BUTTON_PREV;
+const int BUTTON_PAUSE_HOLD = 10 + BUTTON_PAUSE;
+const int BUTTON_INFO_HOLD = 10 + BUTTON_INFO;
+const int BUTTON_AVSYNC_HOLD = 10 + BUTTON_AVSYNC;
+
+void Press(int key, bool shift)
+{
     CGKeyCode inputKeyCode = key;
 
     // Create an HID hardware event source
@@ -24,7 +39,7 @@ void Press(int key, bool shift) {
     // Create a new keyboard key press event
     CGEventRef pressEvent = CGEventCreateKeyboardEvent(source, inputKeyCode, 1);
 
-    if(shift)
+    if (shift)
         CGEventSetFlags(pressEvent, kCGEventFlagMaskShift);
 
     // Create a new keyboard key release event
@@ -44,9 +59,49 @@ void Press(int key, bool shift) {
 
 int main(int argc, char *argv[])
 {
-    sleep(2);
-    Press(kVK_LeftArrow, false);
-    Press(kVK_ANSI_N, true);
-    printf("Hello world!\n");
+    const int buf_max = 256;
+
+    char serialport[buf_max];
+    char eolchar = '\n';
+    int timeout = 5000;
+    char buf[buf_max];
+    int rc, n;
+
+    int fd = serialport_init("/dev/cu.wchusbserial1420", 9600);
+
+    int btn = 0;
+    while (true)
+    {
+        usleep(500 * 1000);
+        for(int i = 0; i < 5; i++){
+            memset(buf,0,buf_max);
+            serialport_read_until(fd, buf, eolchar, buf_max, timeout);
+
+            btn = 0;
+            btn = strtol(buf, NULL, 10);
+
+            switch(btn){
+                case BUTTON_NEXT:
+                    Press(kVK_RightArrow, false);
+                    break;
+                case BUTTON_PREV:
+                    Press(kVK_LeftArrow, false);
+                    break;
+                case BUTTON_PAUSE:
+                    Press(kVK_Space, false);
+                    break;
+                case BUTTON_NEXT_HOLD:
+                    Press(kVK_ANSI_N, true);
+                    break;
+                case BUTTON_PREV_HOLD:
+                    Press(kVK_ANSI_P, true);
+                    break;
+            }
+            usleep(5 * 1000);
+        }
+    }
+
+    serialport_close(fd);
+
     return 0;
 }
