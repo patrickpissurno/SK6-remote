d4c080ffd2e4428e60d18fd64c611561499b5770
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-17 20:51:00 -0300

    Windows version
    - Windows version of the daemon is finally working
    - Still need to convert it into a Windows Service
    - Still need to create a installer package

---
+++ daemon/Windows/Daemon/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Daemon
+{
+    class Program
+    {
+        const int RECEIVER_HANDSHAKE_CODE = 28719;
+
+        const int BUTTON_NEXT = 1;
+        const int BUTTON_PREV = 2;
+        const int BUTTON_PAUSE = 3;
+        const int BUTTON_INFO = 4;
+        const int BUTTON_AVSYNC = 5;
+
+        const int BUTTON_NEXT_HOLD = 10 + BUTTON_NEXT;
+        const int BUTTON_PREV_HOLD = 10 + BUTTON_PREV;
+        const int BUTTON_PAUSE_HOLD = 10 + BUTTON_PAUSE;
+        const int BUTTON_INFO_HOLD = 10 + BUTTON_INFO;
+        const int BUTTON_AVSYNC_HOLD = 10 + BUTTON_AVSYNC;
+
+        static void Press(ScanCodeShort key, bool shift)
+        {
+            INPUT[] inputs = new INPUT[shift ? 2 : 1];
+            INPUT input = new INPUT();
+
+            if (shift)
+            {
+                input.type = 1; // 1 = Keyboard Input
+                input.U.ki.wScan = ScanCodeShort.LSHIFT;
+                input.U.ki.dwFlags = KEYEVENTF.SCANCODE;
+                inputs[0] = input;
+            }
+
+            input.type = 1; // 1 = Keyboard Input
+            input.U.ki.wScan = key;
+            input.U.ki.dwFlags = KEYEVENTF.SCANCODE;
+            inputs[shift ? 1 : 0] = input;
+
+            Input.SendInput((uint)inputs.Length, inputs, INPUT.Size);
+
+            if (shift)
+            {
+                inputs = new INPUT[1];
+                input.type = 1; // 1 = Keyboard Input
+                input.U.ki.wScan = ScanCodeShort.LSHIFT;
+                input.U.ki.dwFlags = KEYEVENTF.SCANCODE | KEYEVENTF.KEYUP;
+                inputs[0] = input;
+                Input.SendInput((uint)inputs.Length, inputs, INPUT.Size);
+            }
+
+            Thread.Sleep(1);
+        }
+
+        static void Main(string[] args)
+        {
+            const int bufferMax = 256;
+            byte[] buf = new byte[bufferMax];
+
+            Console.WriteLine("Daemon Started");
+
+            //wait a bit of time for all components to initialize before trying to connect
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Locating the Receiver device");
+
+            string[] ports;
+            int portsLength;
+            SerialPort port = null;
+            do
+            {
+                Thread.Sleep(1000);
+                ports = SerialPort.GetPortNames();
+                portsLength = ports.Length;
+
+                foreach (var name in ports) {
+                    try
+                    {
+                        port = new SerialPort(name, 9600);
+                        port.ReadTimeout = 2100;
+                        port.DtrEnable = true;
+                        port.Open();
+
+                        if (port.ReadLine().Trim() == RECEIVER_HANDSHAKE_CODE.ToString())
+                            break;
+
+                        throw new Exception();
+                    }
+                    catch
+                    {
+                        portsLength -= 1;
+                        if (port != null)
+                        {
+                            if(port.IsOpen)
+                                port.Close();
+                            port.Dispose();
+                        }
+                        port = null;
+                    }
+                }
+            }
+            while (portsLength < 1);
+
+            if(port == null)
+            {
+                Console.WriteLine("Failed to find a Receiver device");
+
+                Thread.Sleep(10);
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Connected to the Receiver device");
+
+            port.ReadTimeout = 5000;
+
+            int btn = 0;
+            while (true)
+            {
+                Thread.Sleep(500);
+                for (int i = 0; i < 5; i++)
+                {
+                    btn = 0;
+                    
+                    try
+                    {
+                        btn = int.Parse(port.ReadLine().Trim());
+                    }
+                    catch
+                    {
+                        btn = 0;
+                    }
+
+                    if (btn == RECEIVER_HANDSHAKE_CODE)
+                        continue;
+
+                    switch (btn)
+                    {
+                        case BUTTON_NEXT:
+                            Press(ScanCodeShort.RIGHT, false);
+                            break;
+                        case BUTTON_PREV:
+                            Press(ScanCodeShort.LEFT, false);
+                            break;
+                        case BUTTON_PAUSE:
+                            Press(ScanCodeShort.SPACE, false);
+                            break;
+                        case BUTTON_NEXT_HOLD:
+                            Press(ScanCodeShort.KEY_N, true);
+                            break;
+                        case BUTTON_PREV_HOLD:
+                            Press(ScanCodeShort.KEY_P, true);
+                            break;
+                    }
+
+                    Thread.Sleep(5);
+                }
+
+                if (!port.IsOpen)
+                    break;
+            }
+
+            port.Close();
+            port.Dispose();
+
+            Console.WriteLine("Disconnected from the Receiver device");
+
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ daemon/Windows/Daemon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SK6 Remote")]
+[assembly: AssemblyDescription("Make your LG SK6 soundbar remote controller work with your computer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Patrick Pissurno")]
+[assembly: AssemblyProduct("SK6 Remote Daemon")]
+[assembly: AssemblyCopyright("Copyright SK6 Remote © Patrick Pissurno 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4bdf0dd1-1ba1-46dd-b14a-79551cab96da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
